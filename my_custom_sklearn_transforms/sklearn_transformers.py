fc78014708a970ac1371a73f859e6399631eafa1
Baleio369 <50523809+Baleio369@users.noreply.github.com>
2020-08-30 19:18:00 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -13,3 +13,19 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+    
+    #ADICIONEI DAQUI PRA BAIXO:
+    
+    from sklearn.ensemble import RandomForestClassifier
+
+classifier_rf = 
+    RandomForestClassifier(random_state=1986,
+                           criterion='gini',
+                           max_depth=10,
+                           n_estimators=50,
+                           n_jobs=-1)
+scores_rf = cross_val_score(classifier_rf, X, y,
+                            scoring='accuracy', cv=5)
+
+print(scores_rf.mean())
